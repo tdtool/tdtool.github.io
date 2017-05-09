0d605144504e7e2f8d3e157bdc63e61bb9a352df
zhengfeng.yao <zhengfeng.yao@tongdun.cn>
2017-05-09 14:38:12 +0800

    add postinstall

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,11 @@ if which gitbook 2>/dev/null; then
   echo "gitbook installed."
 else
   echo "no gitbook installed. Begin to install."
-  npm i gitbook -g
+  if which yarn 2>/dev/null; then
+    yarn global add gitbook
+  else
+    npm i gitbook -g
+  fi
 fi
 
 cd ./docs
