ab0dad65e7b9dc7af549fb95de78f2f1d64d2f12
xiaofei.wang <979436452@qq.com>
2017-08-14 11:01:55 +0800

    删除无用代码

--- src/cmds/init.js
+++ src/cmds/init.js
@@ -1,28 +1,3 @@
-/**
- * @Author: Zhengfeng.Yao <yzf>
- * @Date:   2017-05-04 09:49:52
- * @Last modified by:   yzf
- * @Last modified time: 2017-05-04 09:49:55
- */
-
-// import fs from 'fs'
-// import path from 'path'
-// import shell from 'shelljs'
-// import logger from '../util/logger'
-//
-// module.exports = function init(options) {
-//   return new Promise((resolve, reject) => {
-//     const argv = process.argv.slice(2);
-//     // shell.exec(`slush ${argv.length >= 2 ? argv[1]: ''}`, {async:true})
-//     // if (shell.exec(`slush ${argv.length >= 2 ? argv[1]: ''}`).code !== 0) {
-//     //   // shell.exit(1);
-//     // } else {
-//     //   // shell.exit(0);
-//     // }
-//   })
-// }
-
-
 var gutil = require('gulp-util');
 var prettyTime = require('pretty-hrtime');
 var glob = require('glob');
